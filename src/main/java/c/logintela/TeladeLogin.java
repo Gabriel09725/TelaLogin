a119beb48206d8683514339d6ab9797c04f422ab
Gabriel09725 <gabrieldemelloo09@gmail.com>
2026-09-11 22:09:00 -0300

    atualizado

---
+++ src/main/java/c/logintela/TeladeLogin.java
@@ -0,0 +1,9 @@
+package c.logintela;
+
+public class TeladeLogin {
+
+    public static void main(String[] args) {
+        view.Telinha tela = new view.Telinha();
+        tela.setVisible(true);
+    }
+}
